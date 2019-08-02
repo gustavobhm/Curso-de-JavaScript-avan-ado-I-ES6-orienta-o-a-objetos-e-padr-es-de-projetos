60c19fc7f386054bde935bd8f45dc96c993c4703
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-08-02 10:03:37 -0300

    Aula 4 - Lidar com data é trabalhoso? Chame um ajudante!

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,6 +7,7 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
 
     }
 
@@ -14,18 +15,25 @@ class NegociacaoController {
 
         event.preventDefault();
 
-        let data = new Date(...this._inputData
-            .value.split('-')
-            .map((item, indice) => item - indice % 2)
-        );
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
 
-        let negociacao = new Negociacao(
-            data,
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value
-        );
+            this._inputValor.value);
+    }
 
-        console.log(negociacao);
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+
+        this._inputData.focus();
     }
 
 }
